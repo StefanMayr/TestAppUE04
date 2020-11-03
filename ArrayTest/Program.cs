9943f142180485c81624d81b6646fb4b91f66ec3
StefanMayr <72915997+StefanMayr@users.noreply.github.com>
2020-11-03 18:57:17 +0100

    Initial commit

---
+++ ArrayTest/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ArrayTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int arraySize = 0;
+            int age = 0;
+            string emptyStr = "";
+            int[] myFirstArray = { 1, 2, 3, 5 };
+            bool isNotTextOnly = false;
+
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+            Console.WriteLine("Good morning dear students!");
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine("today we will talk about arrays!");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+            Console.Write("Bitte geben Sie einen Text ein ");
+            emptyStr = Console.ReadLine();
+
+            // check for text only
+            for (int i = 0; i < emptyStr.Length; i++)
+            {
+                if(int.TryParse(emptyStr[i].ToString(), out age))
+                {
+                    isNotTextOnly = true;
+                    Console.WriteLine($"Ist eine Zahl {emptyStr[i]}");
+                }
+                else
+                {
+                    Console.WriteLine($"Ist keine Zahl {emptyStr[i]}");
+                }
+            }
+
+            do
+            {
+                Console.WriteLine("Bitte geben Sie eine Zahl ein");
+            } while (!int.TryParse(Console.ReadLine(), out arraySize));
+
+            int[] myArray = new int[arraySize];
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                myArray[i] = i + 24;
+            }
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine($"Inhalt von {myArray[i]}");
+            }
+        }
+    }
+}
